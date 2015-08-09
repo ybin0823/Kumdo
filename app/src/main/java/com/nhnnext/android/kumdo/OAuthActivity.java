063217be17e001eda9d7f83af54b8b5ac114e71a
Jangyoungbin <jyb0823@gmail.com>
2015-08-09 17:24:40 +0900

    versionCode="18" versionName="0.1.8"
    -네아로를 통해 로그인이 성공하면 MenuActivity로 전환

--- app/src/main/java/com/nhnnext/android/kumdo/OAuthActivity.java
+++ app/src/main/java/com/nhnnext/android/kumdo/OAuthActivity.java
@@ -1,11 +1,14 @@
 package com.nhnnext.android.kumdo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.nhn.android.naverlogin.OAuthLogin;
 import com.nhn.android.naverlogin.OAuthLoginDefine;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
 
 /**
@@ -38,13 +41,34 @@ public class OAuthActivity extends AppCompatActivity {
         mContext = this;
 
         /*
-		 * 2015년 8월 이전에 등록하고 앱 정보 갱신을 안한 경우 기존에 설정해준 callback intent url 을 넣어줘야 로그인하는데 문제가 안생긴다.
+         * 2015년 8월 이전에 등록하고 앱 정보 갱신을 안한 경우 기존에 설정해준 callback intent url 을 넣어줘야 로그인하는데 문제가 안생긴다.
 		 * 2015년 8월 이후에 등록했거나 그 뒤에 앱 정보 갱신을 하면서 package name 을 넣어준 경우 callback intent url 을 생략해도 된다.
 		 */
         mOAuthLoginInstance = OAuthLogin.getInstance();
         mOAuthLoginInstance.init(mContext, OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_CLIENT_NAME);
 
         mOAuthLoginButton = (OAuthLoginButton) findViewById(R.id.buttonOAuthLoginImg);
-       // TODO Button OAuthLoginHandler 등록
+        mOAuthLoginButton.setOAuthLoginHandler(mOAuthLoginHandler);
     }
+
+    /**
+     * OAuthLoginHandler를 startOAuthLoginActivity() 메서드 호출 시 파라미터로 전달하거나 OAuthLoginButton
+     * 객체에 등록하면 인증이 종료되는 것을 확인할 수 있습니다.
+     */
+    private OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+        @Override
+        public void run(boolean success) {
+            if (success) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+            } else {
+                String errorCode = mOAuthLoginInstance.getLastErrorCode(mContext).getCode();
+                String errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext);
+                Toast.makeText(mContext, "errorCode:" + errorCode
+                        + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        ;
+    };
 }
